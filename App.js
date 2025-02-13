68e56c4621da18d31e872e1d1d5fe99244089478
OrichalcosShunnoros <c3146092091@gmail.com>
2025-02-12 22:15:17 -0500

    I: project initialization.

--- App.js
+++ App.js
@@ -4,16 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Text style={{ 
+        color: '#ccc', 
+        fontSize: 30, 
+        fontFamily: 'System', 
+        fontWeight: 'bold' 
+      }}
+      >
+          Data Flow Analytics
+      </Text>
+      <Text style={{ color: '#ccc', fontSize: 30 }}>In Progress...</Text>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
